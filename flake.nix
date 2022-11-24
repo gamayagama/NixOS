5351b9e2950ed6b26da91607bd437ebe5e0ae850
Fillip Holgersen <fmrh@pm.me>
2022-11-24 11:25:03 +0000

    Removed nur input

--- flake.nix
+++ flake.nix
@@ -4,14 +4,12 @@
   inputs = {
     nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
     home-manager = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs"; };
-    nur = { url = "github:nix-community/NUR"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
   outputs =
   { self
   , nixpkgs
   , home-manager
-  , nur
   , ... } @ inputs:
 
     let
@@ -30,7 +28,7 @@
       );
       homeConfigurations = (
         import ./users {
-         inherit lib system pkgs nur home-manager;
+         inherit lib system pkgs home-manager;
         }
       );
     };
