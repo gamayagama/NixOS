8e087ebcd9552f38875b16cfaed1652f8208fbf3
Fillip Holgersen <fmrh@pm.me>
2023-02-13 10:00:01 +0000

    Restructured audio config slightly

--- hosts/modules/common/audio/default.nix
+++ hosts/modules/common/audio/default.nix
@@ -2,13 +2,17 @@
 
 {
   sound.enable = true;
-  hardware.pulseaudio.enable = false;
+  hardware.pulseaudio = {
+    enable = false;
+  };
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+    audio.enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    wireplumber.enable = true;
     jack.enable = true;
   };
 }
