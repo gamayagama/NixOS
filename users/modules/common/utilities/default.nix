df0ce48b7ef5fc5caf428b47dd478a6a50f3f7a5
Fillip Holgersen <fmrh@pm.me>
2023-04-01 18:10:20 +0100

    Solved issue with flameshot tray unit missing

--- users/modules/common/utilities/default.nix
+++ users/modules/common/utilities/default.nix
@@ -5,6 +5,15 @@
     enable = true;
   };
 
+  # Enable tray unit for flameshot due to a bug not enabling it on Wayland
+  # See more: https://github.com/nix-community/home-manager/issues/2064
+  systemd.user.targets.tray = {
+		Unit = {
+			Description = "Home Manager System Tray";
+			Requires = [ "graphical-session-pre.target" ];
+		};
+	};
+
   programs.mpv = {
     enable = true;
   };
