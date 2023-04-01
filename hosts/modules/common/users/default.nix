04b9374612a6badb38dcbdc53caeed672ed3a3ad
Fillip Holgersen <fmrh@pm.me>
2023-04-01 19:03:50 +0100

    Enabled zsh systemwide temporarily to solve error message, as it did not recognise it already being enabled for the user profile

--- hosts/modules/common/users/default.nix
+++ hosts/modules/common/users/default.nix
@@ -10,4 +10,8 @@ in {
     description = "Fillip Holgersen";
     extraGroups = [ "networkmanager" "wheel" "adbusers" ];
   };
+
+  # Zsh being enabled in HM config is not recognised resulting in an error message
+  # Enabled systemwide to solve this temporarily
+  programs.zsh.enable = true;
 }
