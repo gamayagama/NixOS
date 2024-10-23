502ef9906233200ca5bef6092b44c51441ccd03c
Gamayun Robakov <admin@gama.pm>
2024-10-23 21:15:28 +0200

    clean up

--- hosts/nyx/default.nix
+++ hosts/nyx/default.nix
@@ -21,35 +21,9 @@
 
     # Hardware configuration
     ./hardware.nix 
-    # (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
   services.fwupd.enable = true;
-
-  # boot.initrd = {
-  #   availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
-  #   kernelModules = [ ];
-  # };
-  # boot.kernelModules = [ "kvm-intel" ];
-  # boot.extraModulePackages = [ ];
-  #
-  # fileSystems."/" =
-  #   { device = "/dev/disk/by-uuid/e6b15647-3bd6-4329-bf86-5258009ff36c";
-  #     fsType = "ext4";
-  #   };
-  #
-  # fileSystems."/boot" =
-  #   { device = "/dev/disk/by-uuid/DBC7-E7AC";
-  #     fsType = "vfat";
-  #     options = [ "fmask=0022" "dmask=0022" ];
-  #   };
-  #
-  # swapDevices = [ ];
-  #
-  # networking.useDHCP = lib.mkDefault true;
-  #
-  # nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  # hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   boot = {
     tmp.cleanOnBoot = true;
