35058e9b9933ed19a2a99a52f8b06ffb72fd4dfd
Gamayun Robakov <admin@gama.pm>
2024-11-06 19:50:39 +0100

    Configured Telescope

---
+++ modules/home/editor/config/plugins/telescope.lua
@@ -0,0 +1,15 @@
+local telescope = require'telescope'
+
+
+telescope.setup({
+  extensions = {
+    fzf = {
+      fuzzy = true,                   -- false will only do exact matching
+      override_generic_sorter = true, -- override the generic sorter
+      override_file_sorter = true,    -- override the file sorter
+      case_mode = "smart_case",       -- DEFAULT: "smart_case", can also be "ignore_case" or "respect_case"
+    }
+  }
+})
+
+telescope.load_extension('fzf')
