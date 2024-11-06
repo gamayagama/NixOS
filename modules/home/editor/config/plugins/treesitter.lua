11013c4188912e319161245d284546a3d6788c07
Gamayun Robakov <admin@gama.pm>
2024-11-06 19:43:35 +0100

    Cleanup

--- modules/home/editor/config/plugins/treesitter.lua
+++ modules/home/editor/config/plugins/treesitter.lua
@@ -1,8 +1,7 @@
 require('nvim-treesitter.configs').setup {
   auto_install = false,
-  highlight = { enable = true },
   indent = {
-    enable = true, 
+    enable = true,
     disable = { "python", "yaml" },
   },
   highlight = {
