83c1b8590468aaf152ee778a84298a8acfc6d0ac
Gamayun Robakov <admin@gama.pm>
2024-11-16 17:29:28 +0100

    Display-Manager service failed to start on boot with both lightdm and gdm. Suspected culprit in Nvidia, but for now sddm solved the issue

--- modules/nixos/graphical/default.nix
+++ modules/nixos/graphical/default.nix
@@ -1,18 +1,19 @@
 {
   services = {
     displayManager = {
+      sddm.enable = true;
       defaultSession = "cinnamon";
     };
     # desktopManager.plasma6.enable = true;
     xserver = {
       enable = true;
       desktopManager.cinnamon.enable = true;
-      displayManager = {
-        lightdm = {
-          enable = true;
-          greeters.slick.enable = true;
-        };
-      };
+      # displayManager = {
+        # lightdm = {
+        #   enable = true;
+        #   greeters.slick.enable = true;
+        # };
+      # };
     };
   };
 }
