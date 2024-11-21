904dde71c27136c9589400047f265ab69bff0835
Gamayun Robakov <admin@gama.pm>
2024-11-21 21:02:44 +0100

    Map function for cleaner config + more keybindings for telescope

--- modules/home/editor/config/keybindings.lua
+++ modules/home/editor/config/keybindings.lua
@@ -1,4 +1,15 @@
-local setKm = vim.keymap.set
+function Map(mode, lhs, rhs, opts)
+    local options = { noremap = true, silent = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.keymap.set(mode, lhs, rhs, options)
+end
 
-setKm("n", "<leader>ff", require("telescope.builtin").find_files, {})
+local telescopeBuiltin = require('telescope.builtin')
 
+Map('n', "<leader>ff", telescopeBuiltin.find_files, {})
+Map('n', "<leader>pf", telescopeBuiltin.git_files, {})
+Map('n', "<leader>ps", function()
+  telescopeBuiltin.grep_string({ search = vim.fn.input("Project search > ") })
+end)
