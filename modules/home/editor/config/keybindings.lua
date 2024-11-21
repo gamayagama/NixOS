c230cdcbb418dc5f36a091efec89fefaaf39b965
Gamayun Robakov <admin@gama.pm>
2024-11-21 22:05:11 +0100

    Added comment to address issue with current keybindings, will be resolved at a later time

--- modules/home/editor/config/keybindings.lua
+++ modules/home/editor/config/keybindings.lua
@@ -10,6 +10,8 @@ local telescopeBuiltin = require('telescope.builtin')
 
 Map('n', "<leader>ff", telescopeBuiltin.find_files, { desc = "Find files in directory"})
 Map('n', "<leader>pf", telescopeBuiltin.git_files, { desc = "Project files" })
+-- TODO: this does not search in git project, so keybinding should reflect this
+-- Consider a separate group for git tasks
 Map('n', "<leader>ps", function()
   telescopeBuiltin.grep_string({ search = vim.fn.input("Project search > ") })
 end, { desc = "Project search" })
