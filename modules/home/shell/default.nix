894758275ab8eb0484b87e8df77fa6eb9f558b46
Gamayun Robakov <admin@gama.pm>
2024-11-28 01:44:47 +0100

    Improved terminal experience with various cli tool replacements

--- modules/home/shell/default.nix
+++ modules/home/shell/default.nix
@@ -1,8 +1,10 @@
 let
   myAliases = {
-    ls = "ls -lAh -gS --color";
+    ls = "eza";
+    ll = "eza -la";
     ping = "ping -c 5";
     diff = "diff --color";
+    cat = "bat";
   };
 in {
   programs = {
@@ -16,6 +18,29 @@ in {
     starship = {
       enable = true;
       enableZshIntegration = true;
+    };
+    bat = {
+      enable = true;
+    };
+    eza = {
+      enable = true;
+      enableZshIntegration = true;
+      extraOptions = [
+        "--group-directories-first"
+        "--colour=always"
+        "--icons=auto"
+        "--header"
+      ];
+    };
+    tealdeer = {
+      enable = true;
+      settings = {
+        display = {
+          compact = true;
+          use_pager = false;
+        };
+        updates = {auto_update = true;};
+      };
     };
   };
 }
