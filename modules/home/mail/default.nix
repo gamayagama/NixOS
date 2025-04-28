16d12684f8f4cbaacc36aad6c511ce2eb38e7bc4
Gamayun Robakov <admin@gama.pm>
2025-04-28 21:23:33 +0200

    Added searx as the default search engine

--- modules/home/mail/default.nix
+++ modules/home/mail/default.nix
@@ -4,5 +4,16 @@
   programs.thunderbird = {
     enable = true;
     package = pkgs.thunderbird-latest;
+    profiles.default = {
+      isDefault = true;
+      search = {
+        default = "searx";
+        engines = {
+          # "searx" = {
+          #   urls = [{ template = "https://search.rhscz.eu/search?q={searchTerms}"; }];
+          # };
+        };
+      };
+    };
   };
 }
